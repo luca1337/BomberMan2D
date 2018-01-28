edbfe9115133b2375bc0cb7db5232128387f5209
SamueleAlpino <samuelealpino@gmail.com>
2018-01-28 21:31:19 +0100

    fixed
    correct index

--- BomberMan2D/Prefabs/Map.cs
+++ BomberMan2D/Prefabs/Map.cs
@@ -11,19 +11,19 @@ namespace BomberMan2D.Prefabs
 {
     public class Map : GameObject
     {
-        private int                     blockSize = 50;
-        private static int                     rows;
-        private static int                     columns;
-        private static List<int>               cells;
-        private float                   offset;
-        private static Vector2          playerSpawnPoint;
+        private int blockSize = 50;
+        private static int rows;
+        private static int columns;
+        private static List<int> cells;
+        private float offset;
+        private static Vector2 playerSpawnPoint;
         private static List<Vector2> powerUpSpawnPoints = new List<Vector2>();
+        private bool firstTimeIteration;
 
         public Map(string fileName)
         {
-            //represent blockSize * 2 (2 times blocksize)
-            offset = 2 * blockSize;
-            cells  = new List<int>();
+            cells              = new List<int>();
+            firstTimeIteration = true;
 
             ReadFromFile(fileName);
             GenerateMap();
@@ -35,19 +35,19 @@ namespace BomberMan2D.Prefabs
             {
                 if (cells[iterator] == 3)
                 {
-                    Spawn(new Tile(new Vector2((iterator % (columns - 1) * blockSize), ((iterator / (columns - 1) * blockSize) + offset)), "Wall", true));
+                    Spawn(new Tile(new Vector2((iterator % (columns - 1) * blockSize), ((iterator / (columns - 1) * blockSize))), "Wall", true));
                 }
                 else if (cells[iterator] == 2)
                 {
-                    Spawn(new Tile(new Vector2((iterator % (columns - 1) * blockSize), ((iterator / (columns - 1) * blockSize) + offset)), "Obstacle", true));
+                    Spawn(new Tile(new Vector2((iterator % (columns - 1) * blockSize), ((iterator / (columns - 1) * blockSize) )), "Obstacle", true));
                 }
                 else if (cells[iterator] == 5)
                 {
-                    playerSpawnPoint = new Vector2((iterator % (columns - 1) * blockSize), ((iterator / (columns - 1) * blockSize) + offset));
+                    playerSpawnPoint = new Vector2((iterator % (columns - 1) * blockSize), ((iterator / (columns - 1) * blockSize) ));
                 }
                 else if (cells[iterator] == 5 || cells[iterator] == 0)
                 {
-                    powerUpSpawnPoints.Add(new Vector2((iterator % (columns - 1) * blockSize), ((iterator / (columns - 1) * blockSize) + offset)));
+                    powerUpSpawnPoints.Add(new Vector2((iterator % (columns - 1) * blockSize), ((iterator / (columns - 1) * blockSize) )));
                 }
             }
         }
@@ -59,29 +59,43 @@ namespace BomberMan2D.Prefabs
 
             foreach (string t1 in lines)
             {
-                string[] values = t1.Trim().Split(',',' ');
+                string[] values = t1.Trim().Split(',', ' ');
                 if (columns == 0)
                     columns = values.Length;
+                if (firstTimeIteration)
+                {
+                    //represent the insertion of two rows
 
-               for (int i = 0; i < values.Length; i++)
-               {
-                   int value;
-                   string currentVal = values[i].Trim();
-                   bool success      = int.TryParse(currentVal, out value);
-                   if (success)
+                    for (int i = 0; i < (values.Length - 1) * 2; i++)
+                    {
+                        cells.Add(-10);
+                    }
+                    firstTimeIteration = false;
+                }
+
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    int value;
+                    string currentVal = values[i].Trim();
+                    bool success = int.TryParse(currentVal, out value);
+                    if (success)
                         cells.Add(value);
                 }
             }
+
+
+
         }
 
 
         public static bool GetIndex(bool explosion, int x, int y) // for explosion spawn
         {
-            int index = ((x/50) + (columns - 1) * ((y/50) + 2));
+            int index = (y / 50) * (columns - 1) + (x / 50);
 
             if (explosion)
             {
-                if (cells[index ] == 0 || cells[index] == 5 || cells[index] == 12)
+                if (cells[index] == 0 || cells[index] == 5 || cells[index] == 12)
                     return true;
 
                 if (cells[index] == 3)
